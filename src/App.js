068bdf4223b51c0aae3a309bc99b2357719f9be3
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-09 07:03:45 -0600

    added operator and special buttons. Added display

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
-
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
+import Display from './components/DisplayComponents/Display';
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import { operators } from "./data";
 
 
 
@@ -23,7 +26,14 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <div className="specialAndNumbers">
+        <Specials />
         <Numbers />
+        </div>
+        <div className="operators">
+        <Operators />
+        </div>
       </div>
     </div>
   );
